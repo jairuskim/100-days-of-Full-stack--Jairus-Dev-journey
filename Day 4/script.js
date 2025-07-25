3f768c9d50cc741830fd2ce156518cd3ce48d69b
tchalla001 <jairusgachuhi@gmail.com>
2025-07-26 01:29:27 +0300

    day 4 updating my index.html and starting up on javascript with the script file.

---
+++ Day 4/script.js
@@ -0,0 +1,12 @@
+let name = "jairus";
+let age = 28;
+let gender = "male";
+let isLearning = true;
+let tools = ["HTML", "CSS", "JavaScript"];
+
+console.log("Day 4 of 100 Days of Full-Stack!");
+console.log("Name", name);
+console.log("Learning Full stack?", isLearning);
+console.log("How old are you", age);
+console.log("Gender?" + gender);
+console.log("My Tools", tools);
