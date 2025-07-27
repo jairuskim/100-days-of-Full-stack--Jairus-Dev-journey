35b2ea747adfd04118609d54dd1f81b29f629af4
tchalla001 <jairusgachuhi@gmail.com>
2025-07-28 01:55:13 +0300

    my day 5 commit learning js

---
+++ Day 5/scripts.js
@@ -0,0 +1,19 @@
+// first exercise
+let num = 15;
+
+if (num % 10 === 0) {
+    console.log("Even number")
+}else{
+    console.log("Odd number")
+};
+
+//second exercise
+let age = 18;
+
+if (age > 18){
+    console.log("Access granted")
+}else if(age === 18){
+    console.log("Welcome to adulthood")
+}else{
+    console.log("Access denied")
+}
